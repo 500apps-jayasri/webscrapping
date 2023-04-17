af6edef90b90432901b5f812286a4c986a648878
500apps-pavan <pavan.nidubrolu@500apps.com>
2023-04-17 19:18:02 +0530

    tried for infinity scroll

--- sample/spiders/linkedin.py
+++ sample/spiders/linkedin.py
@@ -5,21 +5,21 @@ class LinkedinSpider(scrapy.Spider):
     name = "linkedin"
     allowed_domains = ["www.linkedin.com"]
     start_urls = ["https://www.linkedin.com/jobs/search?keywords=testing&location=&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"]
-    current_page = 0
-
+    page_num = 0
+    max_pages = 10
+    
     def parse(self, response):
         job_links_data = response.xpath("//ul[@class='jobs-search__results-list']/li/div/a").xpath('@href').getall()
         for job_link in job_links_data:
-            print("========== ", len(job_links_data), "++++++++", job_links_data)
             yield scrapy.Request(url=job_link, callback=self.parse_job, dont_filter=True)
             
-        self.current_page += 1
-        next_url = f"https://www.linkedin.com/jobs/search?keywords=testing&location=&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&currentJobId=3564601980"
-        yield scrapy.Request(url=next_url, callback=self.parse, dont_filter=True)
+        self.page_num += 1
+        if self.page_num < self.max_pages:
+           next_url = response.xpath("//div[@class='top-card-layout__card relative p-2 papabear:p-details-container-padding']/a").xpath('@href').get()
+           yield scrapy.Request(url=next_url, callback=self.parse, dont_filter=True)
     
     
     def parse_job(self, response):
-        job_dict = {}
         title = response.xpath("//div[@class='top-card-layout__entity-info-container flex flex-wrap papabear:flex-nowrap']/div/h1/text()").get()
         company_name = response.xpath("//div[@class='top-card-layout__entity-info-container flex flex-wrap papabear:flex-nowrap']/div/h4/div/span/a/text()").get()
         location = response.xpath("//h4[@class='top-card-layout__second-subline font-sans text-sm leading-open text-color-text-low-emphasis mt-0.5']/div[1]/span[2]/text()").get()
